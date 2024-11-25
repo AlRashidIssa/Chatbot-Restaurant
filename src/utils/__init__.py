bb0700ee0c4e7fa5f8e2b05aba49c593527a4e47
alrashid2001 <alrashidissa2001@hotmail.com>
2024-11-25 01:45:54 +0000

    Change Some error in file __init__.py for Duplicate Loges.

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,37 +1,40 @@
 import logging
 import os
 
-# Define the log Directory relative to this file's loaction
+# Define the log Directory relative to this file's location
 LOG_DIR = os.path.join(os.path.dirname(__file__), "/workspaces/Chatbot-Restaurant/logs")
 LOG_DIR = os.path.abspath(LOG_DIR)
-print("log Directore abspath", LOG_DIR)
+print("Log Directory absolute path:", LOG_DIR)
 
 os.makedirs(LOG_DIR, exist_ok=True)
 
 def setup_logger(name, log_file, level=logging.INFO):
-    """Sets up a logger with specified name, log file, and loggind level."""
+    """Sets up a logger with specified name, log file, and logging level, ensuring no duplicate handlers."""
     logger = logging.getLogger(name)
-    logger.setLevel(level)
+    
+    # Check if the logger already has handlers
+    if not logger.hasHandlers():
+        logger.setLevel(level)
 
-    # Create a file handler for loggind
-    file_handler = logging.FileHandler(log_file)
-    file_handler.setLevel(level)
+        # Create a file handler for logging
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(level)
 
-    # Create a console handler for logging 
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(level)
+        # Create a console handler for logging 
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(level)
 
-    # Define log massage formate
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
+        # Define log message format
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
 
-    # Add handlers to the logger
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
-
+        # Add handlers to the logger
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
     return logger
 
+# Create loggers
 error_log = setup_logger(name="error_log", log_file=os.path.join(LOG_DIR, "error.log"), level=logging.ERROR)
 chatbot_log = setup_logger(name="chatbot_log", log_file=os.path.join(LOG_DIR, "chatbot.log"), level=logging.INFO)
-pipeline_log = setup_logger(name="infenrece_log", log_file=os.path.join(LOG_DIR, "infenrece_log.log"), level=logging.INFO)
+pipeline_log = setup_logger(name="inference_log", log_file=os.path.join(LOG_DIR, "inference_log.log"), level=logging.INFO)
